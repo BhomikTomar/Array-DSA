57eeaf5274d948fe817a8c8444d56d6273c0db73
BhomikTomar <bhomiktomar@gmail.com>
2024-10-13 21:55:51 +0530

    added maxSum_subArray c++ solution

--- maxSum_subArray.cpp
+++ maxSum_subArray.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> maxSum(vector<int>& arr,int n){
+int maxSum(vector<int>& arr,int n){
     bool allNeg = true;
     int maxNeg = INT_MIN;
     for(int i=0;i<n;i++){
@@ -11,7 +11,36 @@ vector<int> maxSum(vector<int>& arr,int n){
     }
     if(allNeg) return {maxNeg};
 
-    
+    int sum = 0;
+    int start = 0;
+    int ansStart = -1;
+    int ansEnd = -1;
+    int maxi = INT_MIN;
+
+    for(int i =0;i<n;i++){
+        if(sum == 0){
+            start = i;
+        }
+
+        sum+=arr[i];
+
+        if(sum > maxi){
+            maxi = sum;
+            ansStart = start;
+            ansEnd = i;
+        }
+
+        if(sum<0){
+            sum = 0;
+        }
+    }
+
+    for(int i = ansStart; i<= ansEnd; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    return maxi;
 }
 int main(){
     int n;
@@ -22,12 +51,10 @@ int main(){
         cin>>arr[i];
     }
 
-    vector<int> ans;
+    int ans;
     ans = maxSum(arr,n);
 
-    for(int i=0;i<ans.size();i++){
-        cout<<ans[i]<<" ";
-    }
+    cout<<ans<<endl;
 
     return 0;
 }
